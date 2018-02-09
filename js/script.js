7adf76c42be2a942278b8df205ac24d1b340d64a
Daniel Burke <burkdan@umich.edu>
2018-02-09 14:24:35 -0500

    fixed issue with modal jumping to top of page when exited with href=#

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
+
+// var dlg_opts = { show: false, keyboard: false, backdrop: 'static' };
+// $('.modal').modal(dlg_opts);
+
 $(document).ready(function () {
     $("div[id^='portfolioModal']").each(function () {
         var currentModal = $(this);
+
         //click next
         currentModal.find('.next').click(function () {
             currentModal.modal('hide');
             //            console.log(currentModal.closest());
-            currentModal.nextAll("div[id^='portfolioModal']").first().modal('show');
+            currentModal.closest("div[id^='portfolioModal']").nextAll("div[id^='portfolioModal']").first().modal('show');
         });
 
         //click prev
         currentModal.find('.prev').click(function () {
             currentModal.modal('hide');
             //            console.log(currentModal.closest("div[id^='portfolioModal']"));
-            currentModal.prevAll("div[id^='portfolioModal']").first().modal('show');
+            currentModal.closest("div[id^='portfolioModal']").prevAll("div[id^='portfolioModal']").first().modal('show');
         });
     });
 });
+
